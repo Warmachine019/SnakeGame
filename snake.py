b42a78bb24a0b8ec5875617e658d409c8a6e8109
Spandan <91448111+Warmachine019@users.noreply.github.com>
2024-08-01 02:08:28 +0530

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
@@ -6,6 +7,7 @@ UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+COLORS = ["red", "blue", "orange", "purple", "white", "green"]
 
 
 class Snake:
@@ -16,11 +18,18 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color(COLORS[0])
+        random.shuffle(COLORS)
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
